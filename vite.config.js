274cf0bef6b41c4c88ddea08ef2d1a9f5d6b7dfa
moniwero <mmonisiabb@gmail.com>
2024-06-23 09:42:54 +0200

    start new project

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+/**
+ * Path to exercise folder
+ */
+const exercisePath = "06_Dzien_3_-_Praca_domowa/03_Strona_eventowa";
+
+/**
+ * Don't change those lines below
+ */
+export default defineConfig({
+  root: exercisePath,
+  server: {
+    port: 3000,
+  },
+});
